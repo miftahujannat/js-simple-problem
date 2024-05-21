c124b0d1eb60fd07e133ccf032d98458f2c8a859
miftahujannat <miftahurimny@gmail.com>
2024-05-21 10:40:08 +0600

    added

---
+++ math.js
@@ -0,0 +1,13 @@
+// const myNumber = -5;
+// const  output = Math.abs(myNumber);
+
+const myNumber = 21.9999;
+// const output = Math.ceil(myNumber);
+// const output = Math.floor(myNumber)
+// const output = Math.round(myNumber)
+for (let i = 0; i <= 30; i++){
+    const output = Math.random() * 6;
+    const rouded = Math.round(output);
+    console.log(rouded);
+
+}
